59e789ec5804361deb1dc6c0de4c09ceb2b20448
Dan Tablac <dctablac@gmail.com>
2021-09-01 22:00:32 -0700

    Confirmed data retrieval from backend. Must do stuff with data.

--- public/js/search.js
+++ public/js/search.js
@@ -1,12 +1,21 @@
-
-// const searchURL = 'api.openweathermap.org/data/2.5/forecast?q=';
+const apiUrl = 'http://dctablac-simple-weather-bckend.herokuapp.com/weather'
 const searchButton = document.getElementById('search-button');
+
 
 searchButton.addEventListener('click', async (e) => {
     e.preventDefault();
     let city = document.getElementById('search-bar').value;
-    // let result = await fetch(`${searchURL}${city}&${process.env.API}`);
-    // Call python API to retrieve results
-    console.log(result);
+    let response = await fetch(`${apiUrl}/${city}`);
+    let data = await response.json();
+    // Do something with the weather data
+    console.log(data);
 });
 
+
+function displayCurrentWeather(currentWeatherDetails) {
+
+}
+
+function displaySevenDayForecast(forecastDetails) {
+
+}
