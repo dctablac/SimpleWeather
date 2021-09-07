2c941d1dad6b4afa3b4ac68cd8279942d40ee880
Dan Christopher Tablac <dctablac@gmail.com>
2021-09-07 10:28:56 -0700

    Basic display of current temperature details, using API icon for now

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,7 @@
 const apiUrl = 'https://dctablac-simple-weather-bckend.herokuapp.com/weather'
+//const apiUrl = "http://127.0.0.1:5000/weather";
 const searchButton = document.getElementById('search-button');
+const openWeatherIconUrl = 'http://openweathermap.org/img/wn/'
 
 
 searchButton.addEventListener('click', async (e) => {
@@ -8,14 +10,28 @@ searchButton.addEventListener('click', async (e) => {
     let response = await fetch(`${apiUrl}/${city}`);
     let data = await response.json();
     // Do something with the weather data
-    console.log(data);
+    if (data.city === null) {
+        alert('Please enter a valid city');
+    } else {
+        console.log(data)
+        displayCurrentWeather(data.city, data.current_weather);
+        displaySevenDayForecast(data.seven_day_forecast)
+    }
 });
 
 
-function displayCurrentWeather(currentWeatherDetails) {
+function displayCurrentWeather(city, currentWeatherDetails) {
+    const iconCode = currentWeatherDetails.weather_description.icon
+    const description = currentWeatherDetails.weather_description.description
+    const temperature = currentWeatherDetails.current_temp
 
+    document.querySelector('#date').innerHTML = Date.now();
+    document.querySelector('#location').innerHTML = city;
+    document.querySelector('#weather-animation-container').innerHTML = `<img src="${openWeatherIconUrl+iconCode+'@2x.png'}"/>`
+    document.querySelector('#weather-temperature').innerHTML = temperature;
+    document.querySelector('#weather-description').innerHTML = description;
 }
 
 function displaySevenDayForecast(forecastDetails) {
-
+    console.log(forecastDetails);
 }
